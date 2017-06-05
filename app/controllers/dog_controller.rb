050f2352be57f752a98a3b955d926ad475e279e1
Talita Matheus Campos <talitamacampos@gmail.com>
2017-06-05 17:34:31 -0300

    Adiciona geração de dados aleatórios de cachorros

---
+++ app/controllers/dog_controller.rb
@@ -0,0 +1,28 @@
+class DogController < ApplicationController
+
+  def index
+    @dogs = Array.new
+
+    (1..1000).each do |i|
+      @dogs.push(generate_dog_data())
+    end
+  end
+
+  private
+  def generate_dog_data
+    # We store the birth in a variable because we will use it to determine the last visit
+    birth = Time.at(rand * (Time.now.to_f))
+
+    data = {
+      'name' => Faker::Cat.name,
+      'breed' => BREEDS[rand(414)],
+      'genre' => (rand(100) % 2 == 1) ? 'male' : 'female',
+      'castrated' => (rand(100) % 2 == 1) ? true : false,
+      'birth' => birth.strftime('%d/%m/%Y'),
+      'owner_name' => Faker::Name.name,
+      'owner_phone' => Faker::PhoneNumber.cell_phone,
+      'last_visit' => Time.at(birth + rand * (Time.now.to_f - birth.to_f)).strftime('%d/%m/%Y')
+    }
+  end
+
+end
